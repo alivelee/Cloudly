c8078d18ea16d1b20ea798ed69cc035652c028bb
Xu Lei <a871460434@gmail.com>
2017-03-16 12:34:33 +0800

    add getIconId method

--- app/src/main/java/club/shfront/cloudly/weather/Forecast.java
+++ app/src/main/java/club/shfront/cloudly/weather/Forecast.java
@@ -1,6 +1,8 @@
 package club.shfront.cloudly.weather;
 
 
+import club.shfront.cloudly.R;
+
 public class Forecast {
     private Current xCurrent;
     private Hourly[] xHourlyForecast;
@@ -28,5 +30,40 @@ public class Forecast {
 
     public void setDailyForecast(Daily[] dailyForecast) {
         xDailyForecast = dailyForecast;
+    }
+
+    public static int getIconId(String xIcon) {
+        int iconId = R.mipmap.clear_day;
+        if (xIcon.equals("clear-day")) {
+            iconId = R.mipmap.clear_day;
+        }
+        else if (xIcon.equals("clear-night")) {
+            iconId = R.mipmap.clear_night;
+        }
+        else if (xIcon.equals("rain")) {
+            iconId = R.mipmap.rain;
+        }
+        else if (xIcon.equals("snow")) {
+            iconId = R.mipmap.snow;
+        }
+        else if (xIcon.equals("sleet")) {
+            iconId = R.mipmap.sleet;
+        }
+        else if (xIcon.equals("wind")) {
+            iconId = R.mipmap.wind;
+        }
+        else if (xIcon.equals("fog")) {
+            iconId = R.mipmap.fog;
+        }
+        else if (xIcon.equals("cloudy")) {
+            iconId = R.mipmap.cloudy;
+        }
+        else if (xIcon.equals("partly-cloudy-day")) {
+            iconId = R.mipmap.partly_cloudy;
+        }
+        else if (xIcon.equals("partly-cloudy-night")) {
+            iconId = R.mipmap.cloudy_night;
+        }
+        return iconId;
     }
 }
