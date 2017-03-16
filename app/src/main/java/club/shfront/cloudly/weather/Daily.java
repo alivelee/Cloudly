de6e952554e3493b46bc7749ce903615e2b24b08
Xu Lei <a871460434@gmail.com>
2017-03-16 12:43:03 +0800

    read parcelable data

--- app/src/main/java/club/shfront/cloudly/weather/Daily.java
+++ app/src/main/java/club/shfront/cloudly/weather/Daily.java
@@ -1,13 +1,20 @@
 package club.shfront.cloudly.weather;
 
 
-public class Daily {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class Daily implements Parcelable {
     private long xTime;
     private String xSummary;
     private double xTempMax;
     private String xIcon;
     private String xTimezone;
-
     public long getTime() {
         return xTime;
     }
@@ -24,8 +31,9 @@ public class Daily {
         xSummary = summary;
     }
 
-    public double getTempMax() {
-        return xTempMax;
+    public int getTempMax() {
+        int FTemp = (int)Math.round(xTempMax);
+        return (int)Math.round((FTemp - 32)/1.8);
     }
 
     public void setTempMax(double tempMax) {
@@ -47,4 +55,51 @@ public class Daily {
     public void setTimezone(String timezone) {
         xTimezone = timezone;
     }
+
+    public int getIconId() {
+        return Forecast.getIconId(xIcon);
+    }
+
+    public String getDayOfTheWeek() {
+        SimpleDateFormat format = new SimpleDateFormat("EEEE", Locale.US);
+        format.setTimeZone(TimeZone.getTimeZone(xTimezone));
+        Date dataTime = new Date(xTime * 1000);
+        return  format.format(dataTime);
+
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeLong(xTime);
+        parcel.writeString(xSummary);
+        parcel.writeDouble(xTempMax);
+        parcel.writeString(xIcon);
+        parcel.writeString(xTimezone);
+    }
+    private Daily(Parcel in) {
+        xTime = in.readLong();
+        xSummary = in.readString();
+        xTempMax = in.readDouble();
+        xIcon = in.readString();
+        xTimezone = in.readString();
+    }
+
+    public Daily (){}
+
+    public static final Creator<Daily> CREATOR = new Creator<Daily>() {
+        @Override
+        public Daily createFromParcel(Parcel parcel) {
+            return new Daily(parcel);
+        }
+
+        @Override
+        public Daily[] newArray(int i) {
+            return new Daily[i];
+        }
+    };
 }
